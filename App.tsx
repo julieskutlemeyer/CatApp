45c872506fe6e09a90f5b9c8bfac553570bc755f
Julie Skutle Meyer <julie.mey@stud.ntnu.no>
2020-11-19 09:38:40 +0100

    Vet ikke hvorfor den ikke hadde riktig fil

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux'
-import AppNavigator from './state-management/AppNavigator'
-import configureStore from "./state-management/state/Store";
+import AppNavigator from './src/AppNavigator'
+import configureStore from "./src/state/Store";
 
 
 export default function App() {
